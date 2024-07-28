bbb16889f7bf9bb9ee16c352ef8de4182d5038be
Aito Murai <aito@outlook.com.au>
2024-07-28 04:55:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Cratesearch.nvim
+Cratesearch.nvim is a plugin for Rustaceans to search [crates.io](https://crates.io/) directly from their Neovim.<br>
+![image goes here](https://picsum.photos/200/300)<br>
+## 🔥 Features
+- Easily configurable, just pass the table of options to the setup function.
+- Easy to use, call ``:CrateSearch [query]`` and you can search.
+## 🚀 Usage
+To use cratesearch, you can use the command ``:CrateSearch [QUERY]``. This will make a HTTP request to crates.io's API (endpoint is https://crates.io/api/v1), and use that data to create a window where you can view the search results. To make this easier to use, in my personal configuration I remapped ``cs`` to ``:CrateSearch``, to allow for easier searching. This is recommended if you don't have anything bound to ``cs`` yet.
+## 📚 Dependencies
+- Neovim >=0.9 (compiled with Lua JIT)
+- Cargo (get it from [here](https://rustup.rs))
+- Curl (should already be installed on most systems)
+## 🛠️ Installation
+<details>
+  <summary>Lazy.nvim</summary>
+
+  ```lua
+  {
+    'Aityz/cratesearch.nvim',
+    config = function()
+        require("cratesearch").setup()
+    end
+  }
+  ```       
+</details>
+<details>
+  <summary>Packer.nvim</summary>
+  
+  ```lua
+  use {
+      'Aityz/cratesearch.nvim',
+  }
+  ```
+</details>
+
+> [!NOTE] 
+> You must call require("cratesearch").setup() for Cratesearch to work, lazy.nvim can do this automatically in the snippet above.
+
+## ⚙️ Configuration
+``cratesearch.nvim`` does not support configuration as of yet.
---
+++ lua/cratesearch/init.lua
@@ -0,0 +1,70 @@
+local T = {}
+
+T.setup = function(opts)
+	local config = opts or {}
+
+	if config.enable == nil or config.enable == false then
+		local search_crate = function(query)
+			local query = query.args
+			local lines = { "Search results for " .. query }
+			local i = 2
+			if query ~= "" then
+				local json_data =
+					vim.json.decode(vim.fn.system("curl -s 'https://crates.io/api/v1/crates?q=" .. query .. "'"))
+
+				local crates = json_data.crates
+
+				for pos, data in pairs(crates) do
+					lines[i] = "["
+						.. pos
+						.. "] "
+						.. data.name
+						.. ": "
+						.. data.description
+						.. " Downloads: "
+						.. data.downloads
+
+					i = i + 1
+				end
+
+				local editor_width = vim.o.columns
+				local editor_height = vim.o.lines
+
+				local width = editor_width - 4
+				local height = 11
+
+				if width > 0 and height > 10 then
+					local row = (editor_height - height) / 2
+					local col = (editor_width - width) / 2
+
+					local options = {
+						height = height,
+						width = width,
+						row = row,
+						col = col,
+						style = "minimal",
+						border = "rounded",
+						relative = "editor",
+					}
+
+					local buf = vim.api.nvim_create_buf(false, false)
+
+					vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
+
+					vim.api.nvim_open_win(buf, true, options)
+
+					vim.bo.modifiable = false
+				else
+					print("Failed to construct window!")
+				end
+			end
+		end
+
+		vim.api.nvim_create_user_command("CrateSearch", search_crate, {
+			nargs = 1,
+			bang = false,
+		})
+	end
+end
+
+return T
